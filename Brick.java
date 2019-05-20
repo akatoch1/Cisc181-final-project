7e276604edbad72a346caa2bea9ee6c88af85223
akatoch1 <44404876+akatoch1@users.noreply.github.com>
2019-05-19 23:05:05 -0400

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -1,18 +1,24 @@
 package cisc181.bustinbricks;
 
+import android.graphics.Canvas;
 import android.graphics.RectF;
+import android.util.Log;
 
 public class Brick {
     private RectF rect;
     private boolean isVisible;
+    public static int scoreArea;
+
+
 
     public Brick(int row, int col, int width, int height) {
         isVisible = true;
         int padding = 1;
+        Log.e("heye", Integer.toString(scoreArea));
         rect = new RectF(col * width + padding,
-                row * height + padding,
+                row * height + padding + scoreArea,
                 col * width + width - padding,
-                row * height + height - padding);
+                scoreArea + row * height + height - padding);
     }
 
     public RectF getRect() {
