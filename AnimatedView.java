8c31308c6c148ed9a4e6eb64aefcf7f6b05d84ad
akatoch1 <44404876+akatoch1@users.noreply.github.com>
2019-05-19 23:03:47 -0400

    Update AnimatedView.java

--- AnimatedView.java
+++ AnimatedView.java
@@ -1,10 +1,15 @@
 package cisc181.bustinbricks;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.util.Log;
+
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -13,20 +18,30 @@ import java.util.ArrayList;
 
 
 public class AnimatedView extends SurfaceView implements SurfaceHolder.Callback{
+
     private MainThread mainThread;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     private Platform platform;
+    Brick[] bricks = new Brick[50];
+    int numBricks = 0;
     private Projectile projectile;
     boolean touchDown = false;
     boolean touchStarted = false;
     float touchX;
-    ArrayList<RectF> bricks = new ArrayList<>();
+    private Lives life1;
+    private Lives life2;
+    private Lives life3;
 
+    ArrayList<Lives> lives = new ArrayList<>();
 
+//scaled bitmap
     public AnimatedView(Context context) {
         super(context);
         getHolder().addCallback(this);
-       mainThread = new MainThread(getHolder(), this);
+        mainThread = new MainThread(getHolder(), this);
         setFocusable(true);
+
     }
 
     @Override
@@ -36,8 +51,23 @@ public class AnimatedView extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        projectile = new Projectile(425, 800);
-        platform = new Platform(375, 1050);
+        int brickWidth = screenWidth / 6;
+        int brickHeight = screenHeight / 20;
+        Brick.scoreArea = screenHeight /16;
+        for(int column = 0; column < 6; column ++ ) {
+            for(int row = 0; row < 3; row ++ ) {
+                bricks[numBricks] = new Brick(row, column, brickWidth, brickHeight);
+                numBricks++;
+            }
+        }
+        projectile = new Projectile(screenWidth/2, (screenHeight*2)/3);
+        platform = new Platform(GameActivity.xScreen, GameActivity.yScreen);
+        life1 = new Lives(BitmapFactory.decodeResource(getResources(),R.drawable.heart), 10);
+        life2 = new Lives(BitmapFactory.decodeResource(getResources(),R.drawable.heart), 90);
+        life3 = new Lives(BitmapFactory.decodeResource(getResources(),R.drawable.heart), 170);
+        lives.add(life1);
+        lives.add(life2);
+        lives.add(life3);
         mainThread.setRunning(true);
         mainThread.start();
 
@@ -52,8 +82,8 @@ public class AnimatedView extends SurfaceView implements SurfaceHolder.Callback{
             try {
                 mainThread.setRunning(false);
                 mainThread.join();
-               // platThread.setRunning(false);
-               // platThread.join();
+                // platThread.setRunning(false);
+                // platThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -69,11 +99,20 @@ public class AnimatedView extends SurfaceView implements SurfaceHolder.Callback{
             projectile.draw(canvas);
             platform.draw(canvas);
             paint.setColor(Color.rgb(0,255,0));
-            for(int i = 0; i < projectile.numBricks; i++) {
-                if(projectile.bricks[i].getVisibility()) {
-                    canvas.drawRect(projectile.bricks[i].getRect(), paint);
+
+            for(int i = 0; i < numBricks; i++) {
+                if(bricks[i].getVisibility()) {
+                    canvas.drawRect(bricks[i].getRect(), paint);
                 }
             }
+            for (int i = 0; i < lives.size(); i++) {
+                lives.get(i).draw(canvas);
+            }
+
+
+
+
+
 
         }
     }
@@ -115,6 +154,38 @@ public class AnimatedView extends SurfaceView implements SurfaceHolder.Callback{
         return true;
     }
     public void update() {
+        RectF ballRect = new RectF(projectile.x, projectile.y, projectile.x, projectile.y );
+        for(int i = 0; i < numBricks; i++){
+            if (bricks[i].getVisibility()){
+                if(RectF.intersects(bricks[i].getRect(), ballRect)) {
+                    if ((ballRect.bottom > bricks[i].getRect().top) && (ballRect.left > bricks[i].getRect().left) && (ballRect.right < bricks[i].getRect().right)) {
+                        projectile.yVelocity *= -1;
+                        bricks[i].setInvisible();
+                        Log.e("hello", "ye");
+
+                    }
+                    else if ((ballRect.top < bricks[i].getRect().bottom)  && (ballRect.left > bricks[i].getRect().left) && (ballRect.right < bricks[i].getRect().right)) {
+                        projectile.yVelocity *= -1;
+                        bricks[i].setInvisible();
+                        Log.e("heye", "ye");
+
+                    }
+                    else {
+                        projectile.xVelocity *= -1 ;
+                        bricks[i].setInvisible();
+                    }
+                    projectile.score++;
+                }
+            }
+        }
+        if(RectF.intersects(platform.getRect(), ballRect)){
+            projectile.yVelocity*=-1;
+        }
+        if (projectile.y >= GameActivity.yScreen) {
+            lives.remove(lives.size() - 1);
+            projectile.x = screenWidth/2;
+            projectile.y = (screenHeight*2)/3;
+        }
         projectile.update();
         platform.update();
     }
